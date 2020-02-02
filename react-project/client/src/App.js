9ed968b3f86e942453e9cfa44413dc6e3e918c27
KathyPuma <kathypuma96@gmail.com>
2020-02-02 17:02:56 -0500

    bunkR project- intitalized from what was left off from the group project.

---
+++ react-project/client/src/App.js
@@ -0,0 +1,375 @@
+// import React from 'react';
+// import axios from 'axios'
+// import Profile from './Components/Profile'
+// import Feed from './Components/Feed'
+// import LogOut from './Components/Logout'
+// import './App.css';
+// import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom'
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       email: "",
+//       exist: false,
+//       id: null,
+//       button: null,
+//       redirected: false,
+//       defaultPhoto: "http://pronksiapartments.ee/wp-content/uploads/2015/10/placeholder-face-big.png",
+//     }
+//   }
+
+//   // componentDidMount = async() => {
+//   //   const checkIfUserIsLoggedIn = await axios.get(`http://localhost:3001/users/logged-in`)
+//   //   let loggedInEmail= checkIfUserIsLoggedIn.data.body
+//   //   // console.log(loggedInEmail)
+//   //   // console.log(loggedInEmail[0])
+//   //   let userInfo = `http://localhost:3001/users/email/${loggedInEmail}`
+//   //   console.log(userInfo)
+//   //   this.setState({
+//   //     email: userInfo
+//   //   })
+//   // }
+//   handleSubmit = (event) => {
+//     event.preventDefault()
+//   }
+//   handleEmailInput = async (event) => {
+//     this.setState({
+//       email: event.target.value
+//     })
+//     // const allImages = await axios.get("http://localhost:3001/images/all")
+//     // console.log("all Images" , allImages)
+//   }
+//   handleLogin = async () => {
+//     const { email, exist, id, button } = this.state
+//     let checkByEmailUrl = `http://localhost:3001/users/email/${email}`
+//     try {
+//       const response = await axios.get(checkByEmailUrl)
+//       let responseData = response.data.body
+//       console.log(responseData)
+//       if (responseData) {
+//         // await axios.post(`http://localhost:3001/users/log-in/${email}`)
+//         return this.setState({
+//           exist: true,
+//           id: responseData.id,
+//           redirected: true,
+//           button: "login",
+//         })
+//       }
+//     } catch (error) {
+//       console.log("Error", error)
+//       this.setState({
+//         button: "login",
+//       })
+//     }
+//   }
+//   handleRegister = async () => {
+//     const { email, exist, id, button, defaultPhoto } = this.state
+//     let checkByEmailUrl = `http://localhost:3001/users/email/${email}`
+//     try {
+//       const response = await axios.get(checkByEmailUrl)
+//       let responseData = response.data.body
+//       if (responseData) {
+//         this.setState({
+//           exist: true,
+//         })
+//       }
+//     } catch (error) {
+//       console.log("Error", error)
+//     }
+//     if (exist === false) {
+//       let addNewUserUrl = "http://localhost:3001/users/sign-up/"
+//       try {
+//         const postNewEmailData = await axios.post(addNewUserUrl, { email: email, img_url: defaultPhoto })
+//         const newUserInfo = await axios.get(checkByEmailUrl)
+//         const newUsersId = newUserInfo.data.body.id
+//         this.setState({
+//           id: newUsersId,
+//           redirected: true,
+//           button: "register"
+//         })
+//       }
+//       catch (error) {
+//         console.log("Error", error)
+//         this.setState({
+//           button: "register"
+//         })
+//       }
+//     }
+//   }
+//   render() {
+//     const { email, exist, id, button, redirected } = this.state
+//     if (button === "login") {
+//       if (exist === true) {
+//         return (
+//           <div className="App">
+//             <nav>
+//               <Link to={`"/profile/${id}`}></Link>{" "}
+//               <Link to={`"/feed/${id}`}></Link>{" "}
+//               <Link to={'/logout/'}></Link>{" "}
+//             </nav>
+//             <Switch>
+//               <Route path="/feed/:id" render={
+//                 (routeProps) => {
+//                   return (
+//                     <Feed email={email} id={id} />
+//                   )
+//                 }
+//               } />
+//               <Route path="/profile/:id" render={
+//                 (routeProps) => {
+//                   return (
+//                     <Profile email={email} id={id} />
+//                   )
+//                 }
+//               } />
+//               <Route path="/logout" component={LogOut}/>
+//             </Switch>
+//             <Redirect to={`/feed/${id}`} ç />
+//           </div>
+//         );
+//       } else {
+//         return (
+//           <div className="App">
+//             <div className="AppStage">
+//               <h1>BunkR</h1>
+//               <div className="login-form">
+//                 <form className="app_form" onSubmit={this.handleSubmit}>
+//                   <p>Email: </p>
+//                   <input className="loginInput" onChange={this.handleEmailInput} type="text" />
+//                   <button className="loginLogin" onClick={this.handleLogin}>Login</button>
+//                   <button className="loginRegister" onClick={this.handleRegister}>Register</button>
+//                 </form>
+//                 <p>Email Does Not Exist. Try again.</p>
+//               </div>
+//             </div>
+//           </div>
+
+//         );
+//       }
+//     } else if (button === "register") {
+//       if (exist === true) {
+//         return (
+//           <div className="App">
+//             <div className="AppStage">
+//               <h1>BunkR</h1>
+//               <div className="login-form">
+//                 <form className="app_form" onSubmit={this.handleSubmit}>
+//                   <p>Email: </p>
+//                   <input className="loginInput" onChange={this.handleEmailInput} type="text" />
+//                   <button className="loginLogin" onClick={this.handleLogin}>Login</button>
+//                   <button className="loginRegister" onClick={this.handleRegister}>Register</button>
+//                 </form>
+//                 <p>Email is Already Registered. </p>
+//               </div>
+//             </div>
+//           </div>
+//         );
+//       } else {
+//         return (
+//           <div className="App">
+//             <nav>
+//               <Link to={`"/profile/${id}`}></Link>{" "}
+//               <Link to={`"/feed/${id}`}></Link>{" "}
+//               <Link to={'/logout/'}></Link>{" "}
+//             </nav>
+//             <Switch>
+//               <Route path="/profile/:id" render={
+//                 (routeProps) => {
+//                   return (
+//                     <Profile email={email} id={id} />
+//                   )
+//                 }
+//               } />
+//               <Route path="/feed/:id" render={
+//                 (routeProps) => {
+//                   return (
+//                     <Feed email={email} id={id} />
+//                   )
+//                 }
+//               } />
+//               <Route path="/logout/" component={LogOut}/>
+//             </Switch>
+//             <Redirect to={`/profile/${id}`} />
+//           </div>
+//         );
+//       }
+//     } else if (redirected === false) {
+//       return (
+//         <div className="App">
+//           <div className="AppStage">
+//             <h1>BunkR</h1>
+//             <div className="login-form">
+//               <form className="app_form" onSubmit={this.handleSubmit}>
+//                 <p>Email: </p>
+//                 <input className="loginInput" onChange={this.handleEmailInput} type="text" />
+//                 <button className="loginLogin" onClick={this.handleLogin}>Login</button>
+//                 <button className="loginRegister" onClick={this.handleRegister}>Register</button>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       );
+//     }
+//   }
+// }
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+import axios from 'axios'
+import Profile from './Components/Profile'
+import Feed from './Components/Feed'
+import LogOut from './Components/Logout'
+import './App.css';
+import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom'
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      email: "",
+      password: "",
+      exist: false,
+      id: null,
+      button: null,
+      redirected: false,
+      defaultPhoto: "http://pronksiapartments.ee/wp-content/uploads/2015/10/placeholder-face-big.png",
+    }
+  }
+
+  // componentDidMount = async() => {
+  //   const checkIfUserIsLoggedIn = await axios.get(`http://localhost:3001/users/logged-in`)
+  //   let loggedInEmail= checkIfUserIsLoggedIn.data.body
+  //   // console.log(loggedInEmail)
+  //   // console.log(loggedInEmail[0])
+  //   let userInfo = `http://localhost:3001/users/email/${loggedInEmail}`
+  //   console.log(userInfo)
+  //   this.setState({
+  //     email: userInfo
+  //   })
+  // }
+  handleSubmit = (event) => {
+    event.preventDefault()
+  }
+  handleEmailInput = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+  handlePassword = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+
+
+
+  handleLogin = async () => {
+    const { email, exist, id, button, password } = this.state
+    try {
+      let checkByEmailUrl = `http://localhost:3001/users/email/${email}/${password}`
+      const response = await axios.get(checkByEmailUrl)
+      let responseData = response.data.body
+      console.log(responseData)
+      if (responseData) {
+        // await axios.post(`http://localhost:3001/users/log-in/${email}`)
+        return this.setState({
+          exist: true,
+          id: responseData.id,
+          redirected: true,
+          button: "login",
+        })
+      } else {
+        this.setState({
+          button: "login",
+        })
+      }
+    } catch (error) {
+      console.log("Error", error)
+      this.setState({
+        button: "login",
+      })
+    }
+  }
+  // handleRegister = async () => {
+  //   const { email, exist, id, button, defaultPhoto } = this.state
+  //   let checkByEmailUrl = `http://localhost:3001/users/email/${email}`
+  //   try {
+  //     const response = await axios.get(checkByEmailUrl)
+  //     let responseData = response.data.body
+  //     if (responseData) {
+  //       this.setState({
+  //         exist: true,
+  //       })
+  //     }
+  //   } catch (error) {
+  //     console.log("Error", error)
+  //   }
+  //   if (exist === false) {
+  //     let addNewUserUrl = "http://localhost:3001/users/sign-up/"
+  //     try {
+  //       const postNewEmailData = await axios.post(addNewUserUrl, { email: email, img_url: defaultPhoto })
+  //       const newUserInfo = await axios.get(checkByEmailUrl)
+  //       const newUsersId = newUserInfo.data.body.id
+  //       this.setState({
+  //         id: newUsersId,
+  //         redirected: true,
+  //         button: "register"
+  //       })
+  //     }
+  //     catch (error) {
+  //       console.log("Error", error)
+  //       this.setState({
+  //         button: "register"
+  //       })
+  //     }
+  //   }
+  // }
+  render() {
+    const { email, exist, id, button, redirected, password } = this.state
+    if (redirected === false || button === "login" && !exist || button === "register" && exist) {
+      return (
+        <div className="App">
+          <div className="AppStage">
+            <h1>BunkR</h1>
+            <div className="login-form">
+              <form className="app_form" onSubmit={this.handleSubmit}>
+                <input className="loginInput" onChange={this.handleEmailInput} type="text" placeholder="Email" value={email} />
+                <input className="loginInput" onChange={this.handlePassword} type="text" placeholder="password" value={password} />
+                <button className="loginLogin" onClick={this.handleLogin}>Login</button>
+                <button className="loginRegister" onClick={this.handleRegister}>Register</button>
+              </form>
+              <div >
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+
+export default App
---
+++ react-project/server/routes/users.js
@@ -0,0 +1,160 @@
+var express = require('express');
+var router = express.Router();
+const db = require('./pgExport');
+
+
+
+router.get("/getAllUsers/", async (req, res, next) => {
+  try {
+    let response = await db.any("SELECT * FROM users");
+    res.json({
+      status: "success",
+      body: response
+    })
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Error: something went wrong"
+    })
+  }
+})
+
+
+// router.get("/logged-in/", async (req, res) => {
+//   try {
+//     let response = await db.one("SELECT email FROM users WHERE loggedIn= true ;", req.body.loggedIn);
+//     res.json({
+//       status: "success",
+//       body: response
+//     })
+//   } catch (error) {
+//     res.status(500).json({
+//       status: "fail",
+//       message: "Error: something went wrong"
+//     })
+//   }
+// })
+
+
+// router.post("/log-in/:email",  async (req, res) => {
+//   let email = req.params.email
+//   try {
+//     let response = await db.any("UPDATE users SET loggedIn = true WHERE email = $1", email)
+//     res.json({
+//       status: "success",
+//       message: `${email} logged in`
+//     })
+//   } catch (error) {
+//     console.log(error)
+//     res.status(500).json({
+//       status: "fail",
+//       message: "Error: something went wrong"
+//     })
+//   }
+// })
+
+
+// router.post("/log-out", async (req, res) => {
+//   try {
+//     let response = await db.any("UPDATE users SET loggedIn = false WHERE loggedIn = true")
+//     res.json({
+//       status: "success",
+//       body: response
+//     })
+//   } catch (error) {
+//     res.status(500).json({
+//       status: "fail",
+//       message: "Error: something went wrong"
+//     })
+//   }
+// })
+
+
+
+// router.post("/sign-up/",  async(req,res) =>{
+//   try {
+//     let insertQuery = `
+//   INSERT INTO users(email, img_url, user_password)
+//   VALUES($1, $2, $3);
+//   `
+//     await db.none(insertQuery, [req.body.email, req.body.img_url, req.body.user_password]);
+//     res.json({
+//       body: req.body,
+//       message: `User registration was successful!`
+//     })
+//   } catch (error) {
+//     res.json({
+//       message: `There was an error!`
+//     })
+//   }
+// })
+
+
+
+// router.get("/email/:email", async(req,res) =>{
+//   let email = req.params.email
+//   try{
+//     let response = await db.one('SELECT *  FROM users WHERE email = $1 AND user_password = $2', [email, req.body.user_password])
+//     res.json({
+//       status: "success",
+//       body: response
+//     })
+//   }catch(error){
+//     res.status(500).json({
+//       status: "fail",
+//       message: "Error: something went wrong"
+//     })
+//   }
+// })
+
+router.get("/email/:email/:password", async(req,res) =>{
+  let email = req.params.email
+  let password = req.params.password
+  try{
+    let response = await db.one('SELECT * FROM users WHERE email = $1 AND user_password = $2', [email, password])
+    res.json({
+      status: "success",
+      body: response
+    })
+  }catch(error){
+    res.status(500).json({
+      status: "fail",
+      message: "Error: something went wrong"
+    })
+  }
+})
+
+router.get("/profilepic/:id", async(req,res) => {
+  let id = req.params.id
+  try {
+    let response = await db.any(`SELECT img_url FROM users WHERE id = $1`, id)
+    res.json({
+      status: "success",
+      body: response
+    })
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Error: something went wrong"
+    })
+  }
+})
+
+
+router.put("/profilepic/:id",  async(req,res) => {
+  let id = req.params.id
+  try {
+    await db.none('UPDATE users SET img_url = $1 WHERE id = $2', [req.body.imgUrl, id])
+    res.json({
+      body: req.body,
+      message: 'Profile picture changed!'
+    })
+  } catch (error) {
+    res.json({
+      message:'There was an error!'
+    })
+  }
+})
+
+
+module.exports = router;
